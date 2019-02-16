b5f1661b85584c166f5a2bd5d80be69532c3fb69
ja3son <ja3son@outlook.com>
2019-02-16 16:17:06 +0800

    add ball entity

---
+++ gllib/src/main/assets/ball_vertex.glsl
@@ -0,0 +1,9 @@
+#version 320 es
+uniform mat4 uMVPMatrix;
+layout (location = 0) in vec3 aPosition;
+out vec3 vPosition;
+
+void main() {
+   gl_Position = uMVPMatrix * vec4(aPosition,1);
+   vPosition = aPosition;
+}
