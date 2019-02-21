2aae0aaf05b76d177cfd79b012b2d087373b1ec3
huzesheng <huzesheng@baidu.com>
2019-02-21 18:49:54 +0800

    rect texture draw

---
+++ gllib/src/main/java/com/ja3son/gllib/entity/TextureRectEntity.kt
@@ -0,0 +1,63 @@
+package com.ja3son.gllib.entity
+
+import android.opengl.GLES32
+import com.ja3son.gllib.util.MatrixState
+import com.ja3son.gllib.util.ShaderUtils
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+
+class TextureRectEntity(val textureId: Int, val sRange: Float, val tRange: Float) : BaseEntity() {
+    init {
+        init()
+    }
+
+    override fun initVertexData() {
+        vCounts = 6
+        val UNIT_SIZE = 0.3f
+        val vertices = floatArrayOf(-4 * UNIT_SIZE, 4 * UNIT_SIZE, 0f, -4 * UNIT_SIZE, -4 * UNIT_SIZE, 0f, 4 * UNIT_SIZE, -4 * UNIT_SIZE, 0f,
+                4 * UNIT_SIZE, -4 * UNIT_SIZE, 0f, 4 * UNIT_SIZE, 4 * UNIT_SIZE, 0f, -4 * UNIT_SIZE, 4 * UNIT_SIZE, 0f)
+
+        val texCoor = floatArrayOf(
+                0f, 0f, 0f, tRange, sRange, tRange,
+                sRange, tRange, sRange, 0f, 0f, 0f
+        )
+
+        verticesBuffer = ByteBuffer.allocateDirect(vertices.size * FLOAT_SIZE)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer()
+                .put(vertices).position(0) as FloatBuffer
+
+        texCoorBuffer = ByteBuffer.allocateDirect(texCoor.size * FLOAT_SIZE)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer()
+                .put(texCoor).position(0) as FloatBuffer
+    }
+
+    override fun initShader() {
+        program = ShaderUtils.createProgram(
+                ShaderUtils.loadFromAssetsFile("triangle_tex_vertex.glsl"),
+                ShaderUtils.loadFromAssetsFile("triangle_tex_fragment.glsl")
+        )
+    }
+
+    override fun initShaderParams() {
+        aPosition = GLES32.glGetAttribLocation(program, "aPosition")
+        aTexCoor = GLES32.glGetAttribLocation(program, "aTexCoor")
+        uMVPMatrix = GLES32.glGetUniformLocation(program, "uMVPMatrix")
+    }
+
+    override fun drawSelf() {
+        GLES32.glUseProgram(program)
+        MatrixState.setInitStack()
+        MatrixState.translate(0f, 0f, 1f)
+        MatrixState.rotate(yAngle, 0f, 1f, 0f)
+        MatrixState.rotate(xAngle, 1f, 0f, 0f)
+        GLES32.glUniformMatrix4fv(uMVPMatrix, 1, false, MatrixState.getFinalMatrix(), 0)
+        GLES32.glVertexAttribPointer(aPosition, posLen, GLES32.GL_FLOAT, false, posLen * FLOAT_SIZE, verticesBuffer)
+        GLES32.glVertexAttribPointer(aTexCoor, texLen, GLES32.GL_FLOAT, false, texLen * FLOAT_SIZE, texCoorBuffer)
+        GLES32.glEnableVertexAttribArray(aPosition)
+        GLES32.glEnableVertexAttribArray(aTexCoor)
+        GLES32.glActiveTexture(GLES32.GL_TEXTURE0)
+        GLES32.glBindTexture(GLES32.GL_TEXTURE_2D, textureId)
+        GLES32.glDrawArrays(GLES32.GL_TRIANGLES, 0, vCounts)
+    }
+}
