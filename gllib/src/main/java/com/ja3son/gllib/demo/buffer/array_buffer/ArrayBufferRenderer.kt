3870e279d59fb56a658d7f321eac739d3ff3400a
huzesheng <huzesheng@baidu.com>
2019-07-31 11:23:01 +0800

    add array buffer drawer

---
+++ gllib/src/main/java/com/ja3son/gllib/demo/buffer/array_buffer/ArrayBufferRenderer.kt
@@ -0,0 +1,49 @@
+package com.ja3son.gllib.demo.buffer.array_buffer
+
+import android.opengl.GLES32
+import com.ja3son.gllib.R
+import com.ja3son.gllib.controller.BaseRenderer
+import com.ja3son.gllib.util.MatrixState
+import com.ja3son.gllib.util.ShaderUtils
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+
+class ArrayBufferRenderer : BaseRenderer() {
+    var textureId = 0
+
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        GLES32.glClearColor(0.5f, 0.5f, 0.5f, 1.0f)
+        GLES32.glEnable(GLES32.GL_DEPTH_TEST)
+        MatrixState.setInitStack()
+        entities = arrayListOf()
+        entities.add(ArrayBufferEntity("ch_t.obj"))
+        entities.add(ArrayBufferEntity("rg.obj"))
+
+        textureId = ShaderUtils.initTexture(R.drawable.qhc)
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        super.onSurfaceChanged(gl, width, height)
+        MatrixState.setProjectFrustum(-ratio, ratio, -1f, 1f, 2f, 1000f)
+        MatrixState.setCamera(0f, 0f, 0f, 0f, 0f, -1f, 0f, 1.0f, 0.0f)
+    }
+
+    override fun onDrawFrame(gl: GL10?) {
+        GLES32.glClear(GLES32.GL_COLOR_BUFFER_BIT or GLES32.GL_DEPTH_BUFFER_BIT)
+        entities[0].xAngle = xAngle
+        entities[0].yAngle = yAngle
+        entities[1].xAngle = xAngle
+        entities[1].yAngle = yAngle
+
+        MatrixState.pushMatrix()
+        MatrixState.translate(-40f, -16f, -150f)
+        entities[0].drawSelf(textureId)
+        MatrixState.popMatrix()
+
+        MatrixState.pushMatrix()
+        MatrixState.translate(40f, -16f, -150f)
+        entities[1].drawSelf(textureId)
+        MatrixState.popMatrix()
+    }
+}
